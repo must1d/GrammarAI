2c1856c61e51c1b3bc8baa675331c8e8b5e90f26
must1d <mustafedobra@outlook.de>
2022-01-23 14:24:01 +0100

    accuracy now optional; improved code in train.py

--- train.py
+++ train.py
@@ -46,11 +46,10 @@ def main(args):
         validation_inputs.append(training_inputs.pop(index))
         validation_targets.append(training_targets.pop(index))
 
-    # list of all batches as tensors
+    # prepare batches as list of tensors
     batches_input_tensors: List[torch.Tensor] = []
     batches_target_tensors: List[torch.Tensor] = []
 
-    # prepare batches and encode inputs and targets
     sequence_size = len(training_inputs[0])
     for i in range(0, len(training_inputs), args.batch_size):
         batch_inputs = training_inputs[i:i+args.batch_size]
@@ -76,23 +75,21 @@ def main(args):
     validation_input_tensor = torch.as_tensor(validation_input_tensor, dtype=torch.float32)
     validation_targets_tensor = torch.as_tensor(validation_targets_tensor, dtype=torch.float32)
 
-    # TODO: add accuracy as metric
     # metrics for plots to track performance in training
     epoch_train_losses = []
     epoch_val_losses = []
-
-    epoch_train_accuracy = []
-    epoch_val_accuracy = []
+    if args.accuracy:
+        print("Warning: Accuracy is enabled! This significantly slows down learning!")
+        epoch_train_accuracy = []
+        epoch_val_accuracy = []
 
     # Training
-    for i in range(args.epochs):
-        num_epoch = i + 1
-        # print("Epoch " + str(i + 1) + " starting:")
-
+    for num_epoch in range(args.epochs):
         # metric variables
         epoch_train_loss = []
-        hits = 0
-        attempts = 0
+        if args.accuracy:
+            hits = 0
+            attempts = 0
 
         # go through all batches
         for j, (batch, target) in enumerate(zip(batches_input_tensors, batches_target_tensors)):
@@ -124,12 +121,14 @@ def main(args):
                 # optimizer.step()
                 loss += loss_fun(predicted, target_2D)
 
-                # compute accuracy
-                for i in range(len(target_2D)):
-                    predicted_char = torch.argmax(target_2D[i]).item()
-                    target_char = torch.argmax(lstm_input[0][i]).item()
-                    hits += 1 if predicted_char == target_char else 0
-                    attempts += 1
+                if args.accuracy:
+                    # TODO: very slow; speedup
+                    # compute accuracy
+                    for i in range(len(target_2D)):
+                        predicted_char = torch.argmax(target_2D[i]).item()
+                        target_char = torch.argmax(lstm_input[0][i]).item()
+                        hits += 1 if predicted_char == target_char else 0
+                        attempts += 1
 
             epoch_train_loss.append(loss.item())
 
@@ -143,13 +142,15 @@ def main(args):
             print_progress_bar(
                 j,
                 len(batches_input_tensors) - 1,
-                f"Epoch {num_epoch}",
+                f"Epoch {num_epoch + 1}",
                 suffix=", " + time_pred_string + "s",
                 fill_char="#"
             )
+
         # Training Metrics
         epoch_train_losses.append(sum(epoch_train_loss)/len(epoch_train_loss))
-        epoch_train_accuracy.append(hits / attempts)
+        if args.accuracy:
+            epoch_train_accuracy.append(hits / attempts)
 
         # Validation
         hits = 0
@@ -173,40 +174,41 @@ def main(args):
             target_2D = validation_targets_tensor[:, i].to(device)
             val_loss += loss_fun(predicted, target_2D)
 
-            # compute accuracy
-            # TODO: Speedup or make optional
-            for i in range(len(target_2D)):
-                predicted_char = torch.argmax(target_2D[i]).item()
-                target_char = torch.argmax(lstm_input[0][i]).item()
-                hits += 1 if predicted_char == target_char else 0
-                attempts += 1
+            if args.accuracy:
+                # compute accuracy
+                for i in range(len(target_2D)):
+                    predicted_char = torch.argmax(target_2D[i]).item()
+                    target_char = torch.argmax(lstm_input[0][i]).item()
+                    hits += 1 if predicted_char == target_char else 0
+                    attempts += 1
 
         # Validation Metrics
         epoch_val_losses.append(val_loss.item())
-        epoch_val_accuracy.append(hits / attempts)
+        if args.accuracy:
+            epoch_val_accuracy.append(hits / attempts)
 
     # Save network
     path = f"models/lstm_e={args.epochs}_bs={args.batch_size}_t={start_time}"
     os.mkdir(path)
     torch.save(lstm.state_dict(), f"{path}/model")
     # Plot
-    plot_training(epoch_train_losses, epoch_val_losses, path, "Losses")
-    plot_training(epoch_train_accuracy, epoch_val_accuracy, path, "Accuracy")
+    plot_metric("Loss", epoch_train_losses, epoch_val_losses, path)
+    if args.accuracy:
+        plot_metric("Accuracy", epoch_train_accuracy, epoch_val_accuracy, path)
 
 
-def plot_training(epoch_train_losses, epoch_val_losses, path, title):
+def plot_metric(metric_name, epoch_train_metric, epoch_val_metric, path):
     plt.clf()
-    plt.title(title)
+    plt.title(metric_name)
     plt.grid(True)
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
-    x_etl = np.arange(1, len(epoch_train_losses) + 1)
-    plt.plot(x_etl, epoch_train_losses, "-r", label="Training")
-    x_evl = np.arange(1, len(epoch_val_losses) + 1)
-    plt.plot(x_evl, epoch_val_losses, "-b", label="Validation")
+    x_etl = np.arange(1, len(epoch_train_metric) + 1)
+    plt.plot(x_etl, epoch_train_metric, "-r", label="Training")
+    x_evl = np.arange(1, len(epoch_val_metric) + 1)
+    plt.plot(x_evl, epoch_val_metric, "-b", label="Validation")
     plt.legend(loc="upper right")
-    plt.savefig(f"{path}/{title}.png")
-    # plt.show()
+    plt.savefig(f"{path}/{metric_name}.png")
 
 
 def loss_fun(pred, target):
@@ -222,5 +224,6 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--lr", type=float, default=0.001)
     parser.add_argument("--val_split", type=float, default=0.1)
+    parser.add_argument("--accuracy", action="store_true")
     args = parser.parse_args()
     main(args)
