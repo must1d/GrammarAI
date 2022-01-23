b9bcb4520e14d49fc46e62e00e1b177d1ad0d72a
JaFuDev <jakoblfuchs@gmx.de>
2022-01-23 01:12:15 +0100

    Fixed prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -1,68 +1,77 @@
-import csv
 import argparse
-from pathlib import Path
-from random import choice
-from typing import List
+import csv
 import json
+import random
+from pathlib import Path
+from typing import List
 
 
 def main(args):
-    sequence_length = args.sequence_length
     sequences: List[str] = []
 
-    # TODO Change sample to some argument
     with open(args.input_path, "r", encoding="utf-8") as infile:
         json_strings = infile.readlines()
-
+        # Read file as lines, one line is one json string
         for json_string in json_strings:
+            # Create json object
             json_obj = json.loads(json_string)
+            # Read plain text from json object
             text = json_obj["text"]
 
-            isInSequence = True
+            inSequence = False
             sequence_counter = 0
-            sequences.append("")
             for i, char in enumerate(text):
-                if sequence_counter >= sequence_length:
-                    isInSequence = False
-                    sequence_counter = 1
-                # Search new upper char
-                if not isInSequence:
-                    if char.isupper():
-                        last_two_characters = text[i-2:i]
-                        if last_two_characters == ". ":
-                            isInSequence = True
-                            sequences.append(char)
-                # Is is input
-                else:
-                    sequences[-1] += char
+                # First, check if the sequence is done
+                if sequence_counter >= args.sequence_length - 1:
+                    # Stop sequence, this char still has the chance to go into a new one
+                    inSequence = False
+                    sequence_counter = 0
+                # Not in a sequence, look for a new start
+                if not inSequence and char.isupper():
+                    last_two_characters = text[i-2:i]
+                    if last_two_characters == ". ":
+                        # Start a sequence
+                        inSequence = True
+                        sequences.append(char)
+                # In a sequence, get the next character
+                elif inSequence:
+                    sequences[-1] += char   # Append character to latest sequence
                     sequence_counter += 1
-
-            if sequences[-1] == "":
-                sequences.pop(-1)
-            if len(sequences[-1]) != args.sequence_length:
+            # Remove last sequence if it's not long enough
+            if len(sequences) > 0 and len(sequences[-1]) < 50:
                 sequences.pop(-1)
 
-    # crate csv output file if it does not exist
+    # Create csv output file if it does not exist
     csv_path = args.output_path / "dataset.csv"
     if not csv_path.exists():
         with open(csv_path, "w", newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
             writer.writerow(["input", "target"])
 
-    # append data to end of csv file
+    # Append data to end of csv file
     with open(csv_path, "a", newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
+        # Prepare actual input for network
+        # Problem: lower/upper string may be longer than original character
         for sequence in sequences:
-            randomized_sequence = ""
-            for c in sequence:
-                if c == "ß":
-                    randomized_sequence += "ß"
+            prepared_sequence = ""
+            # Lower all characters
+            for char in sequence:
+                lowered = char.lower()
+                # uppered = char.upper()
+                if len(char) < len(lowered):
+                    prepared_sequence += char
                 else:
-                    randomized_sequence += choice((str.upper, str.lower))(c)
-
-            if len(randomized_sequence) > 50:
-                print(ra)
-            writer.writerow([randomized_sequence, sequence])
+                    prepared_sequence += lowered
+            # Random capitalization for all characters
+            # for char in sequence:
+            #     lowered = char.lower()
+            #     uppered = char.upper()
+            #     if len(char) < len(lowered) or len(char) < len(uppered):
+            #         prepared_sequence += char
+            #     else:
+            #         prepared_sequence += random.choice((lowered, uppered))
+            # writer.writerow([prepared_sequence, sequence])
 
 
 if __name__ == "__main__":
