be00b53ec13d1d50221c6c1d3fbfc1ef1f23a7fe
must1d <mustafedobra@outlook.de>
2022-01-27 11:51:07 +0100

    Updated main.py to match new training

--- main.py
+++ main.py
@@ -7,15 +7,11 @@ from utils import one_hot, alphabet
 
 def main(args):
     use_cuda = torch.cuda.is_available()
-    if use_cuda:
-        print("Using cuda!")
-    else:
-        print("Using CPU!")
     device = torch.device("cuda" if use_cuda else "cpu")
 
     # load the model
     # TODO: Read hidden size and number of layers from file
-    lstm = LSTM(len(alphabet), 300, 2)
+    lstm = LSTM(len(alphabet), 700, 3)
     lstm.load_state_dict(torch.load(args.model / "model"))
     lstm.eval()
     lstm = lstm.to(device)
@@ -24,7 +20,7 @@ def main(args):
     # prepare the sentence as tensor in onehot encoding
     # [1, sentence_length, one_hot_size]
     sentence = args.sentence
-    sentence_tensor = one_hot(sentence)
+    sentence_tensor = [one_hot(sentence)]
     sentence_tensor = torch.as_tensor(sentence_tensor, dtype=torch.float32)
 
     output_sentence = ""
@@ -33,21 +29,15 @@ def main(args):
     hidden_state, cell_state = lstm.init_lstm_state(1)
     hidden_state = hidden_state.to(device)
     cell_state = cell_state.to(device)
+    lstm_state = (hidden_state, cell_state)
 
-    for i in range(len(sentence)):
-        # input each character as one hot encoding into LSTM
-        lstm_input = sentence_tensor[i, :]
-        lstm_input = lstm_input[None, None, :]  # [1, 1, one_hot_size]
-        lstm_input = lstm_input.to(device)
+    lstm_input = sentence_tensor.to(device)
 
-        lstm_state = (hidden_state, cell_state)
-        predicted, lstm_state = lstm.forward(
-            lstm_input, lstm_state
-        )
+    predicted, __ = lstm.forward(lstm_input, lstm_state)
 
-        # decode result to character
-        index_max = torch.argmax(predicted[0])
-        char = sentence[i] if index_max == 98 else alphabet[index_max]
+    for i, char_pred in enumerate(predicted):
+        index = torch.argmax(char_pred)
+        char = sentence[i] if index == 98 else alphabet[index]
         output_sentence += char
 
     print(output_sentence)
